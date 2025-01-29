e27d8f9a4d643a165aa20c9bb7967d5aff5a287b
Matias <lit.io30303@gmail.com>
2025-01-28 23:12:38 -0300

    update data json

--- brain_mapu/extraer_data.py
+++ brain_mapu/extraer_data.py
@@ -8,22 +8,24 @@ import sys
 # Agregar el directorio raíz al sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from utils.client_llm import cliente_llm
-
-
-
-
-
-cliente = cliente_llm()
-
-
 
 
 def clean_mapudungun(text):
     # Normaliza acentos y caracteres especiales
     text = unidecode(text).lower()
-    # Elimina números/caracteres extraños (personaliza según tus PDFs)
-    return re.sub(r'[^a-zñü/s]', '', text)
+
+    # Elimina caracteres extraños pero mantiene espacios
+    text = re.sub(r'[^a-zñü\s]', '', text)
+
+    # Reemplaza saltos de línea y tabulaciones con espacios
+    text = text.replace('\n', ' ').replace('\t', ' ')
+
+    # Elimina espacios adicionales
+    text = re.sub(r'\s+', ' ', text).strip()    
+
+    return text
+
+
 
 
 def pdf_to_json(pdf_path):
@@ -38,25 +40,23 @@ def pdf_to_json(pdf_path):
             if "lines" in block:
                 for line in block["lines"]:
                     for span in line["spans"]:
+                        #print(span["text"])
                         texto = clean_mapudungun(span["text"])
                         # Acumula todo el texto en una lista
-                        all_text.append(texto + " ")
+                        all_text.append(texto)
 
     combined_text = " ".join(all_text).strip()
 
     file_name = os.path.splitext(os.path.basename(pdf_path))[0]
     #print(file_name)
-    # Estructura final con una sola clave "Data"
+    
     structured_data = {file_name: combined_text}
 
     return structured_data
 
 
 # Lista de archivos PDF
-pdf_files = ["data/Diccionario_mapudungun.pdf", "data/Diccionario-mapudungun-espanol-espanol-mapudungun.pdf"]
-
-
-#data = pdf_to_json("data/Diccionario_mapudungun.pdf")
+pdf_files = ["data_pdf/Diccionario_mapudungun.pdf", "data_pdf/Diccionario-mapudungun-espanol-espanol-mapudungun.pdf"]
 
 
 
@@ -71,12 +71,14 @@ for pdf_file in pdf_files:
     all_data.append(data)
 
 
+#print(all_data[0]['Diccionario_mapudungun'])
+
 
 
 if __name__ == "__main__":
 
     try:
-        ruta_final = "D:/LLLIT/Code-W11/PY/api_bot_bd/temp_calendar/mapu/mapu.json"
+        ruta_final = "D:/LLLIT/Code-W11/PY/api_data_mapu/brain_mapu/json_data/mapuche_data.json"
 
 
         # Guardar los datos en un archivo JSON
